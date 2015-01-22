d334792335bd643c15dff4a6378fd69851eca3df
Tony Malagisi <mtb3000gt@gmail.com>
2015-01-21 23:24:32 -0500

    Update baseband and fingerprint
    Change-Id: Ia99e28c30175585e2c1582b8ebf66a5be603b0f9

--- liquid.mk
+++ liquid.mk
@@ -6,7 +6,11 @@ $(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 # Inherit some common Liquid stuff.
 $(call inherit-product, vendor/liquid/config/common_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2usc TARGET_DEVICE=d2usc BUILD_FINGERPRINT="samsung/d2usc/d2usc:4.1.1/JRO03L/R530UVXALK5:user/release-keys" PRIVATE_BUILD_DESC="d2usc-user 4.1.1 JRO03L R530UVXALK5 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=d2usc \
+    TARGET_DEVICE=d2usc \
+    BUILD_FINGERPRINT="samsung/d2usc/d2usc:5.0.2/LRX22G/R530UVXUBMJA:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2usc-user 5.0.2 LRX22G R530UVXUBMJA release-keys"
 
 PRODUCT_NAME := liquid_d2usc
 PRODUCT_DEVICE := d2usc
